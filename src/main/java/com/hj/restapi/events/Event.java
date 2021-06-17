7f982866d9e17f96b7b958030c710bd48e3680df
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-06-17 23:25:47 +0900

    POST /api/events returns 201

--- src/main/java/com/hj/restapi/events/Event.java
+++ src/main/java/com/hj/restapi/events/Event.java
@@ -2,6 +2,12 @@ package com.hj.restapi.events;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -15,9 +21,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
 
+	@Id @GeneratedValue
 	private Integer id;
+
 	private String name;
 	private String description;
 	private LocalDateTime beginEnrollmentDateTime;
@@ -30,6 +39,8 @@ public class Event {
 	private int limitOfEnrollment;
 	private boolean offline;
 	private boolean free;
+
+	@Enumerated(EnumType.STRING)
 	private EventStatus eventStatus;
 
 }
--- src/main/java/com/hj/restapi/events/EventController.java
+++ src/main/java/com/hj/restapi/events/EventController.java
@@ -10,16 +10,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @Controller
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+@RequiredArgsConstructor
 public class EventController {
+
+	private final EventRepository eventRepository;
 
 	@PostMapping
 	public ResponseEntity createEvent(@RequestBody Event event) {
+		Event newEvent = eventRepository.save(event);
 
-		URI createUri =  linkTo(EventController.class).slash("{id}").toUri();
+		URI createUri =  linkTo(EventController.class).slash(newEvent.getId()).toUri();
 		event.setId(10);
 		return ResponseEntity.created(createUri).body(event);
 	}
