799211f76bda8f11471cf8cb2070534a9c9a67d4
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-06-17 23:31:09 +0900

    polishing

--- src/test/java/com/hj/restapi/events/EventControllerTests.java
+++ src/test/java/com/hj/restapi/events/EventControllerTests.java
@@ -2,6 +2,7 @@ package com.hj.restapi.events;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -54,6 +56,8 @@ public class EventControllerTests {
 						.content(objectMapper.writeValueAsString(event)))
 						.andDo(print())
 						.andExpect(status().isCreated())		//or    is(201)
-						.andExpect(jsonPath("id").exists());
+						.andExpect(jsonPath("id").exists())
+						.andExpect(header().exists(HttpHeaders.LOCATION))
+						.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
 	}
 }
