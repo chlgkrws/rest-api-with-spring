742def229a7335c1da759efcd3df8976f563c32e
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-06-18 17:50:52 +0900

    Remove unnecessary import

--- src/main/java/com/hj/restapi/events/EventController.java
+++ src/main/java/com/hj/restapi/events/EventController.java
@@ -4,7 +4,6 @@ import java.net.URI;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
