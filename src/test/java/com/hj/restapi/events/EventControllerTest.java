7f06c912c27a96e09fe2e2c7917630e5a99210fb
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-06-20 22:27:06 +0900

    fail on unknown properties

--- src/test/java/com/hj/restapi/events/EventControllerTest.java
+++ src/test/java/com/hj/restapi/events/EventControllerTest.java
@@ -60,4 +60,31 @@ public class EventControllerTest {
 						.andExpect(jsonPath("free").value(Matchers.not(true)));
 
 	}
+
+	@Test
+	public void createEvent_bad_request() throws Exception{
+		Event event = Event.builder()
+				.name("Spring")
+				.description("REST API Development with Spring")
+				.beginEnrollmentDateTime(LocalDateTime.of(2021, 6, 16, 12, 29))
+				.closeEnrollmentDateTime(LocalDateTime.of(2021, 6, 17, 12, 29))
+				.beginEventDateTime(LocalDateTime.of(2021, 6, 18, 12, 29))
+				.endEventDateTime(LocalDateTime.of(2021, 6, 19, 12, 29))
+				.basePrice(100)
+				.maxPrice(200)
+				.free(true)
+				.offline(false)
+				.limitOfEnrollment(100)
+				.location("종로 3가역")
+				.build();
+
+		mockMvc.perform(post("/api/events")
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaTypes.HAL_JSON)
+						.content(objectMapper.writeValueAsString(event)))
+						.andDo(print())
+						.andExpect(status().isBadRequest())				//or    is(201)
+
+		;
+	}
 }
